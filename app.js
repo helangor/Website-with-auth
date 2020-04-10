66cf8321795b3acd8632a85d51cbf3a6a3ebff1b
helangor <henrikki_helander@hotmail.com>
2020-04-11 00:01:01 +0300

    Added password encrypting

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
@@ -14,10 +14,14 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+const secret ="Thisisourlittlesecret."
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -35,6 +39,7 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
   const newUser = new User({
+    email: req.body.username,
     password:req.body.password
   });
 
