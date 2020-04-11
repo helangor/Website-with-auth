28ce3bf2aaa993ca6b1637c39a395ab3210544ee
helangor <henrikki_helander@hotmail.com>
2020-04-11 21:47:44 +0300

    Added Facebook auth

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
@@ -36,7 +37,9 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -59,16 +62,27 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userInfo"
+    callbackURL: "http://localhost:3000/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
 ));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FB_APP_ID,
+    clientSecret: process.env.FB_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -85,6 +99,16 @@ app.get('/auth/google/secrets',
     res.redirect("/secrets");
   });
 
+  app.get('/auth/facebook',
+    passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/secrets');
+    });
+
 app.get("/login", function(req, res) {
   res.render("login");
 });
@@ -94,11 +118,42 @@ app.get("/register", function(req, res) {
 });
 
 app.get("/secrets", function(req, res){
+User.find({"secret": {$ne: null}}, function(err, foundUsers){
+  if (err){
+    console.log(err);
+  } else {
+    if (foundUsers) {
+      res.render("secrets", {usersWithSecrets: foundUsers});
+    }
+  }
+});
+});
+
+app.get("/submit", function(req, res) {
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+console.log(req.user);
+
+  User.findById(req.user.id, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 });
 
 app.get("/logout", function(req, res){
@@ -143,3 +198,6 @@ app.post("/login", function(req, res) {
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+// Lisää facebook login
